444aac010f432c76696764dd5381ac246a99a4a3
kyu-kuanwei <v-kyu@microsoft.com>
2021-03-01 14:39:02 +0800

    Export to csv file

--- scratch/scratch.py
+++ scratch/scratch.py
@@ -1,4 +1,5 @@
 import ast
+import datetime
 import os
 import time
 
@@ -7,15 +8,22 @@ import pandas as pd
 from decouple import config
 from selenium import webdriver
 
-from .common import Common
+from .enum import ErrorMessage, SleepTime
 
 
 class Scratch:
 
+    _DATA_PATH = "data"
+    _SCRATCH_FILE_NAME = "data"
+    _TODAY_DATE = str(datetime.date.today())
+
     def __init__(self):
+        self._SCRATCH_DATA_FILE = os.path.join(
+            self._DATA_PATH, "_".join([self._SCRATCH_FILE_NAME, self._TODAY_DATE]) + ".csv"
+        )
         self._load_env()
-        self._browser = webdriver.Chrome(executable_path=self._EXECUTABLE_PATH)
-        self._scratch()
+        # self._scratch()
+        self._check_data()
         self._clean_dataframe()
 
     def _load_env(self):
@@ -25,11 +33,19 @@ class Scratch:
         self._FACEBOOK_PASSWORD = config('FACEBOOK_PASSWORD')
         self._WEB_URL = config('URL')
 
+    def _check_data(self):
+        if os.path.exists(self._SCRATCH_DATA_FILE):
+            self._nba_data = pd.read_csv(self._SCRATCH_DATA_FILE)
+        else:
+            self._scratch()
+
     def _scratch(self):
+        self._browser = webdriver.Chrome(executable_path=self._EXECUTABLE_PATH)
+
         # Open UDN fantansy website.
         try:
             self._browser.get(self._WEB_URL)
-            time.sleep(Common.SHORT_SLEEP_TIME)
+            time.sleep(SleepTime.SHORT_SLEEP_TIME)
         except BaseException:
             print(f"Could not access {self._WEB_URL}")
             exit(101)
@@ -38,9 +54,9 @@ class Scratch:
         try:
             connect_fb_button = self._browser.find_element_by_class_name('btn-fb')
             connect_fb_button.click()
-            time.sleep(Common.SHORT_SLEEP_TIME)
+            time.sleep(SleepTime.SHORT_SLEEP_TIME)
         except BaseException:
-            print("Could not open the Facebook login page.")
+            print(ErrorMessage.FACEBOOK_PAGE_OPEN_ERROR)
             exit(102)
 
         # Login to Facebook.
@@ -50,19 +66,20 @@ class Scratch:
             password = self._browser.find_element_by_name('pass')
             password.send_keys(self._FACEBOOK_PASSWORD)
             login_button = self._browser.find_element_by_name('login')
-            time.sleep(Common.SHORT_SLEEP_TIME)
+            time.sleep(SleepTime.SHORT_SLEEP_TIME)
             login_button.click()
-            time.sleep(Common.LONG_SLEEP_TIME)
+            time.sleep(SleepTime.LONG_SLEEP_TIME)
         except BaseException:
-            print("Could not login to the Facebook account.")
+            print(ErrorMessage.FACEBOOK_LOGIN_ERROR)
             exit(103)
 
         # Start playing.
         try:
             start = self._browser.find_element_by_class_name('btn-play')
             start.click()
+            time.sleep(SleepTime.SHORT_SLEEP_TIME)
         except BaseException:
-            print("Could not access the playing page.")
+            print(ErrorMessage.START_PLAYING_ERROR)
             exit(104)
 
         #Scrath results.
@@ -74,13 +91,19 @@ class Scratch:
             # Close browser.
             self._browser.close()
         except BaseException:
-            print("Could not scratch the player stats.")
+            print(ErrorMessage.SCRATCH_ERROR)
             exit(105)
 
         # Build dataframe.
         self._nba_data = pd.DataFrame(nba_state)
         # Filter out injured players.
         self._nba_data = self._nba_data[~self._nba_data.playerId.isin(injured_players)]
+
+        self._export_to_csv()
+
+    def _export_to_csv(self):
+        # Export to a cache csv file.
+        self._nba_data.to_csv(self._SCRATCH_DATA_FILE, index=False)
 
     def _clean_dataframe(self):
         # Merge players name.
