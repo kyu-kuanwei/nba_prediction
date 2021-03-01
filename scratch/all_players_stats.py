5602a866e3e20fb517a3707661066f7abaa19b61
kyu-kuanwei <v-kyu@microsoft.com>
2021-03-02 01:07:19 +0800

    Refactor

--- scratch/all_players_stats.py
+++ scratch/all_players_stats.py
@@ -7,11 +7,9 @@ from nba_api.stats.endpoints import leaguedashplayerstats
 
 class AllPlayerStats:
 
-    def __init__(self, scratch_results: pd.DataFrame):
+    def __init__(self):
         self._load_configs()
         self._all_players = self._find_players()
-        self._tomorrow_players = scratch_results
-        self._merge_data()
 
     def _load_configs(self):
         with open('config.yml') as f:
@@ -36,14 +34,9 @@ class AllPlayerStats:
             + all_players['BLK'] * 3
             - all_players['TOV']
         )
-        # all_players.sort_values(by='SCR', ascending=False, inplace=True, ignore_index=True)
 
         return all_players.round(2)
 
-    def _merge_data(self):
-        self._valid_players = pd.merge(self._tomorrow_players, self._all_players, on='PLAYER_NAME')
-        self._valid_players.columns = map(str.upper, self._valid_players.columns)
-
     @property
-    def valid_players(self):
-        return self._valid_players
+    def all_players(self):
+        return self._all_players
